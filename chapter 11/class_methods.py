84885e4657b2dbae099ec4c71d5f83480ac68c50
garvmanchal <manchalgarv06@gmail.com>
2026-03-15 22:11:05 +0530

    Chapter 11 updated

--- chapter 11/class_methods.py
+++ chapter 11/class_methods.py
@@ -10,25 +10,43 @@
 # e.a = 78
 
 #property decorators
-class Employee:
-    a = 10 
-    @classmethod
-    def show(cls):
-        print(f"the class attribute value of a is {cls.a}")
+# class Employee:
+#     a = 10 
+#     @classmethod
+#     def show(cls):
+#         print(f"the class attribute value of a is {cls.a}")
 
-    @property
-    def name(self):
-        return f"{self.fname}{self.lname}"
+#     @property
+#     def name(self):
+#         return f"{self.fname}{self.lname}"
         
     
-    @name.setter
-    def name(self,value):
-        self.fname = value.split(" ")[0]
-        self.lname = value.split(" ")[1]
+#     @name.setter
+#     def name(self,value):
+#         self.fname = value.split(" ")[0]
+#         self.lname = value.split(" ")[1]
 
     
-e = Employee()
-e.a = 78
-e.name = "grvv Manchal"
-print(e.fname,e.lname)
-e.show()
+# e = Employee()
+# e.a = 78
+# e.name = "grvv Manchal"
+# print(e.fname,e.lname)
+# e.show()
+
+#operator overloading
+
+class Number :
+    def __init__(self,n):
+        self.n = n
+
+    def __add__(self, num):
+        return self.n + num.n
+    
+    def __len__(self):
+        return len(str(self.n))
+
+n = Number(1)
+m = Number(2)
+
+print(n + m)
+print(len(n))
